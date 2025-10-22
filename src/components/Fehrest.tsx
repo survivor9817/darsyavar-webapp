06e4e75c7840791a4adde86b7855bc58a765223a
Reza <rezaghezelseflou@gmail.com>
2025-10-22 22:48:52 +0330

    class haaye sidebar ezaafe shodand. hanooz daakhele menu saakhte nashode.

--- src/components/Fehrest.tsx
+++ src/components/Fehrest.tsx
@@ -1,5 +1,81 @@
-const Fehrest = () => {
-  return <div>Fehrest</div>;
+import CloseBtn from "./CloseBtn";
+import { bookNames } from "../data/booksData.js";
+
+interface Props {
+  bookName: string;
+  onClose: () => void;
+  onChange: () => void;
+  style: object;
+}
+
+const Fehrest = ({ bookName, onClose, onChange, style }: Props) => {
+  const bookItems = bookNames.map((bookName) => (
+    <option key={bookName} value={bookName}>
+      {bookName}
+    </option>
+  ));
+
+  return (
+    <>
+      <div className="sidebar sidebar-right" style={style}>
+        <CloseBtn onClick={onClose} />
+
+        <div className="fehrest-section">
+          <header className="fehrest-header">
+            <label htmlFor="BookSelector" className="book-selector-label">
+              فهرست کتاب
+            </label>
+            <select
+              id="BookSelector"
+              name="BookSelector"
+              className="book-selector"
+              value={bookName}
+              onChange={onChange}
+            >
+              {bookItems}
+            </select>
+          </header>
+          <ol id="fehrestList" className="fehrest-list">
+            <li>
+              <div className="chapter">something</div>
+              <ol className="subsections">
+                <li>
+                  <div className="article">something</div>
+                </li>
+                <li>
+                  <div className="article">something</div>
+                </li>
+                <li>
+                  <div className="article">something</div>
+                </li>
+              </ol>
+            </li>
+            <li>
+              <div className="chapter">something</div>
+              <ol className="subsections">
+                <li>
+                  <div className="article">something</div>
+                </li>
+                <li>
+                  <div className="article">something</div>
+                </li>
+                <li>
+                  <div className="article">something</div>
+                </li>
+              </ol>
+            </li>
+            <li>
+              <div className="chapter">something</div>
+            </li>
+            <li>
+              <div className="chapter">something</div>
+            </li>
+            {/* <FehrestList fehrestData={fehrest} /> */}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Fehrest;
